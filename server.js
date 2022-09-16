6243635d631c29d27bc545f3b6cef7057023b026
Viny Selopal <viny0698@gmail.com>
2022-09-16 15:36:46 +0530

    added database routes. Not yet tested the server.

--- server.js
+++ server.js
@@ -2,17 +2,70 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = 8000
-const redis = require('redis')
-const client = redis.createClient('127.0.0.1', 6379)
-console.log('in file')
-client.on('error', function (err) {
-  console.log('Redis connection error:', err)
+
+const { insertTodo, getAllTodos, updateTodo, deleteTodo, deleteAll, deleteDone } = require('./database/routes')
+
+app.use(express.json())
+
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'PUT', 'DELETE', 'POST']
+  })
+)
+
+app.post('/', async (req, res) => {
+  try {
+    const result = await insertTodo(req.body)
+    res.json(result)
+  } catch (err) {
+    console.log(err)
+  }
 })
-async function connectRedis () {
-  await client.connect()
-  await client.set('projectname', 'todoApp')
-}
-connectRedis()
+app.get('/', async (req, res) => {
+  try {
+    const result = await getAllTodos()
+    res.json(result)
+  } catch (err) {
+    console.log(err)
+  }
+})
+app.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const result = await updateTodo({ _id: ObjectId(id) }, req.body)
+    res.json(result)
+  } catch (err) {
+    console.log(err)
+  }
+})
+app.delete('/', async (req, res) => {
+  try {
+    const result = await deleteAll()
+    res.json(result)
+  } catch (err) {
+    console.log(err)
+  }
+})
+app.delete('/done', async (req, res) => {
+  try {
+    const result = await deleteDone(req.body)
+    res.json(result)
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+app.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const result = await deleteTodo({ id: ObjectId(id) })
+    res.json(result)
+  } catch (err) {
+    console.log(err)
+  }
+})
+
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
 })
