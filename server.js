28eef652eb7e785d892185a91e79e680615920fb
Viny Selopal <viny0698@gmail.com>
2022-09-16 15:13:24 +0530

    conenction with Redis success

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 8000
 const redis = require('redis')
-const client = redis.createClient()
+const client = redis.createClient('127.0.0.1', 6379)
 console.log('in file')
-client.on('connect', function () {
-  console.log('Connected to Redis Server!')
+client.on('error', function (err) {
+  console.log('Redis connection error:', err)
+})
+async function connectRedis () {
+  await client.connect()
+  await client.set('projectname', 'todoApp')
+}
+connectRedis()
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
 })
